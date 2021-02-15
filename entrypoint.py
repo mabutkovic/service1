e17ff760ac89cbca5687936ca174aabfc33142d4
Marijan Butkovic-Cinnamon <marijan.butkovic-ext@evenfinancial.com>
2021-02-15 16:37:18 +0100

    tried encode

--- entrypoint.py
+++ entrypoint.py
@@ -6,6 +6,6 @@ hash_func = inp[0].strip()
 message = '\n'.join(inp[1:]).strip()
 
 h = hashlib.new(hash_func)
-h.update(str.encode(message))
+h.update(str.encode(message, 'utf-8'))
 
 print h.hexdigest()
