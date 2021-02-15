5ee85542c8c0787e034163c5a6901db03ed571d1
Marijan Butkovic-Cinnamon <marijan.butkovic-ext@evenfinancial.com>
2021-02-15 19:23:56 +0100

    removed encode

--- entrypoint.py
+++ entrypoint.py
@@ -6,6 +6,6 @@ hash_func = inp[0].strip()
 message = '\n'.join(inp[1:]).strip()
 
 h = hashlib.new(hash_func)
-h.update(str.encode(message.decode('utf-8'), 'utf-8'))
+h.update(str.encode(message))
 
 print h.hexdigest()
