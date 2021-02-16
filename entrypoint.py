e625f4d4a2192b02ba3dacafc49b83f0396a6a6a
Marijan Butkovic-Cinnamon <marijan.butkovic-ext@evenfinancial.com>
2021-02-16 08:21:21 +0100

    added decode

--- entrypoint.py
+++ entrypoint.py
@@ -6,6 +6,7 @@ hash_func = inp[0].strip()
 message = '\n'.join(inp[1:]).strip()
 
 h = hashlib.new(hash_func)
+message = message.decode('iso-8859-2'')
 h.update(str.encode(message))
 
 print h.hexdigest()
