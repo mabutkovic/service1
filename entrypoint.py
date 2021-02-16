6c4af2421a0de9ab6b57938fb396b65e3ef4746a
Marijan Butkovic-Cinnamon <marijan.butkovic-ext@evenfinancial.com>
2021-02-16 08:35:18 +0100

    fixed3

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,14 @@
 import sys
 import hashlib
+import sys
+
+sys.setdefaultencoding('utf-8')
 
 inp = sys.stdin.readlines()
 hash_func = inp[0].strip()
 message = '\n'.join(inp[1:]).strip()
 
 h = hashlib.new(hash_func)
-message = message.decode('iso-8859-2')
-h.update(str(message))
+h.update(str.encode(message))
 
 print h.hexdigest()
